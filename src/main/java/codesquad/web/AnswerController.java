95fdcdbe21e6524c82a4c9112e989eb9fc0d66d6
gunjuKo <gunju@sk.com>
2018-02-02 09:45:08 +0900

    refactoring (step3) : 불필요한 어노테이션 제거

--- src/main/java/codesquad/web/AnswerController.java
+++ src/main/java/codesquad/web/AnswerController.java
@@ -7,7 +7,6 @@ import codesquad.security.LoginUser;
 import codesquad.service.QnaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 
-@Validated
 @Controller
 @RequestMapping("/questions/{questionId}/answers")
 public class AnswerController {
--- src/main/java/codesquad/web/api/ApiAnswerController.java
+++ src/main/java/codesquad/web/api/ApiAnswerController.java
@@ -7,7 +7,6 @@ import codesquad.security.LoginUser;
 import codesquad.service.QnaService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@Validated
 @RestController
 @RequestMapping("/api/questions/{questionId}/answers")
 public class ApiAnswerController {
